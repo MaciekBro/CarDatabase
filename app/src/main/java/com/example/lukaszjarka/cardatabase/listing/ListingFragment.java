430d0edd36622907a156661953567d0300bcff88
Maciej Brodowski <maciekbrodowski@gmail.com>
2017-03-27 18:38:57 +0200

    my init

---
+++ app/src/main/java/com/example/lukaszjarka/cardatabase/listing/ListingFragment.java
@@ -0,0 +1,13 @@
+package com.example.lukaszjarka.cardatabase.listing;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by RENT on 2017-03-27.
+ */
+
+public class ListingFragment extends Fragment {
+
+
+
+}
